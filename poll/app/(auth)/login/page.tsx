f95abc76592053f238ca1fabae65217991c82555
Hackante <kante.hagen@gmail.com>
2025-07-03 16:30:33 +0200

    fix: bug that prevented user from entering data

--- poll/app/(auth)/login/page.tsx
+++ poll/app/(auth)/login/page.tsx
@@ -4,8 +4,28 @@ import { PrimaryButtonWithArrowRight } from "@/app/ui-components/buttons/Primary
 import FloatingLabelInput from "@/app/ui-components/inputs/FloatingLabelInput";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    console.log("Form submitted:", formData);
+  };
+
   return (
     <div className="flex h-screen p-24">
       <div className="w-1/3 flex items-center flex-col justify-center">
@@ -19,9 +39,21 @@ export default function LoginPage() {
           </p>
           <div className="flex flex-col space-y-4">
             {/* <FloatingLabelInput label="Username" value="" /> */}
-            <FloatingLabelInput label="Email" value="" type="email" />
-            <FloatingLabelInput label="Password" value="" type="password" />
-            <PrimaryButtonWithArrowRight label="Login" type="submit" />
+            <FloatingLabelInput
+              label="Email"
+              value={formData.email}
+              type="email"
+              name="email"
+              onChange={handleChange}
+            />
+            <FloatingLabelInput
+              label="Password"
+              value={formData.password}
+              type="password"
+              name="password"
+              onChange={handleChange}
+            />
+            <PrimaryButtonWithArrowRight label="Login" type="submit" onClick={handleSubmit} />
           </div>
         </form>
         <hr className="w-100 text-gray-600 m-8"></hr>
