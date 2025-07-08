b2f00a7ec68c1ff56ad7826ae31595089f5ad5f0
Prettier - GitHub Action <actions@github.com>
2025-07-08 20:44:05 +0000

    style: auto-format code with Prettier

--- poll/eslint.config.mjs
+++ poll/eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
     ...compat.extends("next/core-web-vitals", "next/typescript"),
     {
         ignores: ["lib/generated/**/*", "node_modules/**/*"],
-    }
+    },
 ];
 
 export default eslintConfig;
