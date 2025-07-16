157e63fac047091a9fdc32460926761ee6885e30
Prettier - GitHub Action <actions@github.com>
2025-07-16 18:08:06 +0000

    style: auto-format code with Prettier

--- poll/app/dashboard/settings/page.tsx
+++ poll/app/dashboard/settings/page.tsx
@@ -1,50 +1,71 @@
 import { Inter } from "next/font/google";
 import ProfileSettingInput from "@/app/ui-components/inputs/ProfileSettingInput";
-import { ChangePasswordForm } from "@/app/ui-components/forms/change-password-form"
+import { ChangePasswordForm } from "@/app/ui-components/forms/change-password-form";
 
 const inter = Inter({
-  subsets: ["latin"],
+    subsets: ["latin"],
 });
 
 export default function SettingsDashboard() {
-  const userData = {
-    username: "User1234",
-    email: "User1234@example.com",
-    country: "CountryName",
-    bio: "This is the UI design of a pollcreation application",
-  }
+    const userData = {
+        username: "User1234",
+        email: "User1234@example.com",
+        country: "CountryName",
+        bio: "This is the UI design of a pollcreation application",
+    };
 
-  return (
-    <section className="bg-panel-background m-4 flex min-h-screen flex-col rounded-2xl p-6">
-      <h4 className="text-2xl font-bold">Settings</h4>
+    return (
+        <section className="bg-panel-background m-4 flex min-h-screen flex-col rounded-2xl p-6">
+            <h4 className="text-2xl font-bold">Settings</h4>
 
-      <section className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6 mt-[4.125rem]">
-        {/* === User details section === */}
-        <div className="bg-cards-background min-h-screen rounded-xl p-6">
-          <h3 className={`${inter.className} text-cards-foreground font-bold`}>Profile</h3>
+            <section className="mt-[4.125rem] grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6">
+                {/* === User details section === */}
+                <div className="bg-cards-background min-h-screen rounded-xl p-6">
+                    <h3
+                        className={`${inter.className} text-cards-foreground font-bold`}
+                    >
+                        Profile
+                    </h3>
 
-          <div className="mt-[2.188rem] flex flex-col gap-[2.188rem]">
-            {/* === Username input === */}
-            <ProfileSettingInput label="Username" data={userData.username} />
+                    <div className="mt-[2.188rem] flex flex-col gap-[2.188rem]">
+                        {/* === Username input === */}
+                        <ProfileSettingInput
+                            label="Username"
+                            data={userData.username}
+                        />
 
-            {/* === Email input === */}
-            <ProfileSettingInput label="Email" data={userData.email} />
+                        {/* === Email input === */}
+                        <ProfileSettingInput
+                            label="Email"
+                            data={userData.email}
+                        />
 
-            {/* === Country input === */}
-            <ProfileSettingInput label="Country" data={userData.country} />
+                        {/* === Country input === */}
+                        <ProfileSettingInput
+                            label="Country"
+                            data={userData.country}
+                        />
 
-            {/* === Bio input === */}
-            <ProfileSettingInput label="Bio" data={userData.bio} />
-          </div>
-        </div>
+                        {/* === Bio input === */}
+                        <ProfileSettingInput label="Bio" data={userData.bio} />
+                    </div>
+                </div>
 
-        {/* === Password settings section === */}
-        <div className="bg-cards-background min-h-screen rounded-xl p-6">
-          <h3 className={`${inter.className} text-cards-foreground font-bold`}>Change Password</h3>
+                {/* === Password settings section === */}
+                <div className="bg-cards-background min-h-screen rounded-xl p-6">
+                    <h3
+                        className={`${inter.className} text-cards-foreground font-bold`}
+                    >
+                        Change Password
+                    </h3>
 
-          <p className={`${inter.className} font-medium mt-[0.875rem] lg:max-w-[80%]`}>To change password enter your new password and confirm</p>
-        </div>
-      </section>
-    </section>
-  );
+                    <p
+                        className={`${inter.className} mt-[0.875rem] font-medium lg:max-w-[80%]`}
+                    >
+                        To change password enter your new password and confirm
+                    </p>
+                </div>
+            </section>
+        </section>
+    );
 }
--- poll/app/ui-components/inputs/ProfileSettingInput.tsx
+++ poll/app/ui-components/inputs/ProfileSettingInput.tsx
@@ -1,30 +1,34 @@
-"use client"
+"use client";
 
 import { EditIcon } from "@/public/svgs/edit";
 import { Inter } from "next/font/google";
 
 interface ProfileSettingInputProps {
-  label: string;
-  data: string;
+    label: string;
+    data: string;
 }
 
 const inter = Inter({
-  subsets: ["latin"],
-})
+    subsets: ["latin"],
+});
 
 export default function ProfileSettingInput({
-  label,
-  data
- }: ProfileSettingInputProps) {
-  return (
-    <div>
-      <p className="font-medium text-[0.75rem] text-cards-foreground">{label}</p>
-      <div className="flex gap-[1.5rem] items-center mt-[0.375rem]">
-        <p className={`${inter.className} font-bold max-w-[65%]`}>{data}</p>
-        <div >
-          <EditIcon />
+    label,
+    data,
+}: ProfileSettingInputProps) {
+    return (
+        <div>
+            <p className="text-cards-foreground text-[0.75rem] font-medium">
+                {label}
+            </p>
+            <div className="mt-[0.375rem] flex items-center gap-[1.5rem]">
+                <p className={`${inter.className} max-w-[65%] font-bold`}>
+                    {data}
+                </p>
+                <div>
+                    <EditIcon />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
