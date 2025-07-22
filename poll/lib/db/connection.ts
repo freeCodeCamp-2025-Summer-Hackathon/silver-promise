0d900aab0d0b043b2e212b5dca5d8c600a7d1f70
Prettier - GitHub Action <actions@github.com>
2025-07-22 22:59:29 +0000

    style: auto-format code with Prettier

--- poll/lib/db/connection.ts
+++ poll/lib/db/connection.ts
@@ -2,8 +2,7 @@ import { Pool } from "pg";
 require("dotenv").config();
 
 const connectionPool = new Pool({
-  connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL,
 });
 
 module.exports = connectionPool;
-
