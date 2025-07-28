c689e68c2ff6a470a43fcd33a80d15386c0768a3
Prettier - GitHub Action <actions@github.com>
2025-07-28 15:17:06 +0000

    style: auto-format code with Prettier

--- poll/app/dashboard/polls/create-poll.tsx
+++ poll/app/dashboard/polls/create-poll.tsx
@@ -272,7 +272,12 @@ export const CreatePoll = () => {
             (pollTypeValue === PollType.MULTIPLE &&
                 multipleChoiceOptions.length === 0)
         ) {
-            alert("Please fill in the question and at least " + (pollTypeValue === PollType.SINGLE ? "2 options" : "1 option"));
+            alert(
+                "Please fill in the question and at least " +
+                    (pollTypeValue === PollType.SINGLE
+                        ? "2 options"
+                        : "1 option")
+            );
             return;
         }
 
